f610f57fc897439f0d94b5b7b4d46c32bef030f6
Helio Perroni Filho <mail@xperroni.me>
2016-11-30 11:27:27 -0200

    Bottleneck training implementation
    Signed-off-by: Helio Perroni Filho <mail@xperroni.me>

--- feature_extraction.py
+++ feature_extraction.py
@@ -2,15 +2,20 @@ import pickle
 import tensorflow as tf
 # TODO: import Keras layers you need here
 
+from datasets import Dataset, Data, Likelihoods
+from layers import reshape2d, Connected
+from networks import train, Network
+
 flags = tf.app.flags
 FLAGS = flags.FLAGS
 
 # command line flags
 flags.DEFINE_string('training_file', '', "Bottleneck features training file (.p)")
 flags.DEFINE_string('validation_file', '', "Bottleneck features validation file (.p)")
+flags.DEFINE_integer('breadth', 10, "Number of output classes")
 
 
-def load_bottleneck_data(training_file, validation_file):
+def load_bottleneck_data(training_file, validation_file, breadth):
     """
     Utility function to load bottleneck features.
 
@@ -20,6 +25,7 @@ def load_bottleneck_data(training_file, validation_file):
     """
     print("Training file", training_file)
     print("Validation file", validation_file)
+    print("Output breadth", breadth)
 
     with open(training_file, 'rb') as f:
         train_data = pickle.load(f)
@@ -31,23 +37,40 @@ def load_bottleneck_data(training_file, validation_file):
     X_val = validation_data['features']
     y_val = validation_data['labels']
 
-    return X_train, y_train, X_val, y_val
+    D_train = Dataset('Training', Data(X_train), Likelihoods(y_train, breadth))
+    D_val = Dataset('Validation', Data(X_val), Likelihoods(y_val, breadth))
+
+    return (D_train, D_val)
+
+
+def bottleneck_architecture(input_shape, n_hidden, n_classes, nonlinear=tf.nn.relu):
+    inputs = tf.placeholder(tf.float32, (None,) + input_shape)
+    outputs = Connected(reshape2d(inputs), n_hidden, 1.0, nonlinear)
+    outputs = Connected(outputs, n_classes, 1.0)
+    return (inputs, outputs)
 
 
 def main(_):
     # load bottleneck data
-    X_train, y_train, X_val, y_val = load_bottleneck_data(FLAGS.training_file, FLAGS.validation_file)
+    (D_train, D_val) = load_bottleneck_data(FLAGS.training_file, FLAGS.validation_file, FLAGS.breadth)
 
-    print(X_train.shape, y_train.shape)
-    print(X_val.shape, y_val.shape)
+    print(D_train.X.shape, D_train.y.shape)
+    print(D_val.X.shape, D_val.y.shape)
 
     # TODO: define your model and hyperparams here
     # make sure to adjust the number of classes based on
     # the dataset
     # 10 for cifar10
     # 43 for traffic
+    network = Network(
+        bottleneck_architecture,
+        input_shape=D_train.X.shape[1:],
+        n_hidden=64,
+        n_classes=FLAGS.breadth
+    )
 
     # TODO: train your model here
+    train(network, D_train, D_val, learning_rate=0.01, epochs=100)
 
 
 # parses flags and calls the `main` function above
